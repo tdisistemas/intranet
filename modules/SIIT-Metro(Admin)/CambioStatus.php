577da64a1100522955d789fe91579b8034bed6e8
gcarrasquero <gcarrasquero@gcarrasquero-H61H2-CM>
2015-11-26 15:57:11 -0430

    Signed-off-by:gcarrasquero <gcarrasquero@gcarrasquero-H61H2-CM>

--- modules/SIIT-Metro(Admin)/CambioStatus.php
+++ modules/SIIT-Metro(Admin)/CambioStatus.php
@@ -24,7 +24,7 @@ if (isset($_GET['acc'])) {
             $mensaje = "Finalizada";
             Break;
         case 9:
-            $mensaje = "Archivadada";
+            $mensaje = "Archivada";
             break;
     }
     echo $mensaje;
